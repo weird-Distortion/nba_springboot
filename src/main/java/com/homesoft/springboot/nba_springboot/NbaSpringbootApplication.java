c581781d23ff8d4812a8c67b39a3a4e4d565cf71
weird-Distortion <ilyalilison@gmail.com>
2018-10-30 12:08:54 +0100

    initial commit

---
+++ src/main/java/com/homesoft/springboot/nba_springboot/NbaSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.homesoft.springboot.nba_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaSpringbootApplication.class, args);
+    }
+}
---
+++ src/test/java/com/homesoft/springboot/nba_springboot/NbaSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.homesoft.springboot.nba_springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NbaSpringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
